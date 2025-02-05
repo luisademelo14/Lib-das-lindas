191201723172d7f1641dae965041df56cf4ac569
Luana <luanarj30@gmail.com>
2025-02-05 02:17:03 -0300

    update

--- data-structures/lichao-tree.cpp
+++ data-structures/lichao-tree.cpp
@@ -1,4 +1,3 @@
-int n, ini, s[MAXN], f[MAXN], dp[MAXN], tam, idx;
 Func st[MAXN<<2];
 
 void build(int idx = 1, int i = 0, int j = MAXN-1)
@@ -33,29 +32,3 @@ int query(int pos, int idx = 1, int i = 0, int j = MAXN-1)
 	if (pos <= mid) return min(st[idx].val(pos), query(pos, esq(idx), i, mid));
 	return min(st[idx].val(pos), query(pos, dir(idx), mid+1, j));
 }
-
-
-int32_t main()
-{
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	cin >> n >> f[0];
-	build();
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> s[i];
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> f[i];
-	}
-
-	update({f[0], 0});
-	for (int i = 1; i <= n; i++)
-	{
-		dp[i] = query(s[i]);
-		update({f[i], dp[i]});
-	}
-
-	cout << dp[n] << endl;
-}
-
